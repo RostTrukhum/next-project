12f95d2a333fd1dbbca9b6a29ac886458e014a77
anastasia-iteric <anastasia.kyrylenko@iteric.com>
2023-05-28 13:00:02 +0300

    fix link

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 };
 
 const getData = async () => {
-  const response = await fetch('https://jsonplaceholder.typicode.com/postss', {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
     next: {
       revalidate: 60,
     },
